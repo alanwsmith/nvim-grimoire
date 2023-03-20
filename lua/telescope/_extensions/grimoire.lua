a9a2a7e08ee32e78a9bcb8cd83d0bfe8da520e9a
Alan Smith <commit@alanwsmith.com>
2023-03-20 17:28:55 -0400

    Opening basic table

--- lua/telescope/_extensions/grimoire.lua
+++ lua/telescope/_extensions/grimoire.lua
@@ -1,8 +1,15 @@
 local pickers = require "telescope.pickers"
 local finders = require "telescope.finders"
 
-local search_grimoire = function()
-  pickers.new({}, {
+local run_search = function(opts)
+  opts = opts or {}
+  if next(opts) == nil then
+    -- TODO: Figure out how to do a "not" check
+  else
+   require('notify')(opts)
+  end
+
+  pickers.new(opts, {
     prompt_title = "grimoire",
     finder = finders.new_table {
       results = { "red", "green", "blue" }
@@ -10,8 +17,24 @@ local search_grimoire = function()
   }):find()
 end
 
+
+-- local run_search = function(query_string)
+--   local Job = require 'plenary.job'
+--   local msg = require 'notify'
+--   Job:new({
+--     command = 'security',
+--     args = { 'find-generic-password', '-w', '-a', 'alan', '-s', 'alan--meilisearch--scratchpad--test-key' },
+--     on_exit = function(job, return_val)
+--       --run_query(job:result(), query_string)
+--       -- msg(job:result())
+--       -- msg(query_string)
+--     end,
+--   }):start()
+-- end
+--
+--
 return require("telescope").register_extension {
   exports = {
-    grimoire = search_grimoire
+    grimoire = run_search
   },
 }
