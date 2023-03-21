191e9c3bd87b7074db51618cb5faf6135a1372ff
Alan Smith <commit@alanwsmith.com>
2023-03-20 20:05:57 -0400

    Getting a live update happening from the command line and pulling a password

--- lua/telescope/_extensions/grimoire.lua
+++ lua/telescope/_extensions/grimoire.lua
@@ -1,40 +1,62 @@
 local pickers = require "telescope.pickers"
 local finders = require "telescope.finders"
+local Job = require 'plenary.job'
 
-local run_search = function(opts)
-  opts = opts or {}
-  if next(opts) == nil then
-    -- TODO: Figure out how to do a "not" check
-  else
-   require('notify')(opts)
-  end
+local msearch = function()
+	-- local msg = require('notify')
+	-- local window_list = vim.api.nvim_list_wins()
+	-- for i = 1, #window_list do
+	--    msg(vim.win_getid(window_list[i]))
+	--end
+	-- return vim.api.nvim_list_bufs()
+	--  ?
+	-- require('notify')(vim.api.nvim_list_wins())
 
-  pickers.new(opts, {
-    prompt_title = "grimoire",
-    finder = finders.new_table {
-      results = { "red", "green", "blue" }
-    },
-  }):find()
+	--
+	--
+	return { "a", "b", "c" }
 end
 
+local run_search = function()
+	local auth_key = ""
 
--- local run_search = function(query_string)
---   local Job = require 'plenary.job'
---   local msg = require 'notify'
---   Job:new({
---     command = 'security',
---     args = { 'find-generic-password', '-w', '-a', 'alan', '-s', 'alan--meilisearch--scratchpad--test-key' },
---     on_exit = function(job, return_val)
---       --run_query(job:result(), query_string)
---       -- msg(job:result())
---       -- msg(query_string)
---     end,
---   }):start()
--- end
---
---
+	Job:new({
+		-- command = 'date',
+		 command = 'security',
+		args = { 'find-generic-password', '-w', '-a', 'alan', '-s', 'test--example--key' },
+		on_exit = function(job, return_val)
+			-- msg('alfa')
+			--run_query(job:result(), query_string)
+			--return { "e", "q" }--
+			--auth_key = job:result()
+		        auth_key = job:result()[1]
+			-- auth_key = 'asdfasdfasdf'
+			print('asdfasdfasdfsdf')
+			--require('notify')('asdf')
+			-- require('notify')(job:result())
+		end
+	}):sync()
+
+	pickers.new({}, {
+		prompt_title = "grimoire",
+		finder = finders.new_job(function(prompt)
+			return { 'echo', auth_key }
+		end
+		)
+		-- sorter = sorters.highlighter_only(opts),
+		--   --results = { "red", "green", "blue" }
+		-- results = msearch()
+		-- finder = finders.new_table {
+		-- 	--   --results = { "red", "green", "blue" }
+		-- 	results = msearch()
+		-- }
+		-- finder = finders.new_oneshot_job({ 'date' }, opts)
+		-- finder = finders.new_oneshot_job({ 'echo' }, opts)
+	}):find()
+end
+
 return require("telescope").register_extension {
-  exports = {
-    grimoire = run_search
-  },
+	exports = {
+		grimoire = run_search
+	},
 }
