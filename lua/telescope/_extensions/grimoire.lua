4366a3ef1f723fa373aeb3b2e4afe5976ed91a84
Alan Smith <commit@alanwsmith.com>
2023-03-20 22:24:37 -0400

    Got the files lined up to the specific location

--- lua/telescope/_extensions/grimoire.lua
+++ lua/telescope/_extensions/grimoire.lua
@@ -4,10 +4,29 @@ local finders = require "telescope.finders"
 local run_search = function()
 	pickers.new({}, {
 		prompt_title = "grimoire",
-		finder = finders.new_job(function(prompt)
-			return { '/Users/alan/.cargo/bin/grimoire_search', prompt }
-		end
-		)
+		finder = finders.new_job(
+
+			function(prompt)
+				return { '/Users/alan/.cargo/bin/grimoire_search', prompt }
+			end,
+
+ 	function(entry)
+ 		return {
+ 			value    = entry,
+ 			-- value    = entry,
+ 			display  = 't ' .. entry,
+ 			-- display  = entry,
+ 			-- This filename didn't work
+			filename = '/Users/alan/Desktop/' .. entry,
+ 			ordinal  = entry,
+ 		}
+ 	end
+			 -- '_' ,
+
+			 -- '/Users/alan/Desktop'
+
+
+		),
 	}):find()
 end
 
@@ -16,3 +35,18 @@ return require("telescope").register_extension {
 		grimoire = run_search
 	},
 }
+--
+--
+-- {
+-- 	entry_maker = function(entry)
+-- 		return {
+-- 			value    = 'z' .. entry[1],
+-- 			-- value    = entry,
+-- 			display  = 'x' .. entry[1],
+-- 			-- display  = entry,
+-- 			-- This filename didn't work
+-- 			filename = '/Users/alan/Desktop/' .. entry[1],
+-- 			ordinal  = entry,
+-- 		}
+-- 	end
+-- }
